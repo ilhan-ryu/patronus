1895e8ab550947553f00f19ea493a3124d00c3e5
앙리 <mail@1han.kr>
2018-06-17 15:25:01 +0900

    frontend commit

---
+++ frontend/src/components/App/presenter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Footer from "components/Footer";
+import { Route, Switch } from "react-router-dom";
+import "./styles.scss";
+import Auth from "components/Auth";
+
+const App = props => [
+    props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+    <Footer key={3} />
+];
+
+App.PropTypes = {
+    isLoggedIn: PropTypes.bool.isRequired
+}
+
+const PrivateRoutes = props => (
+    <Switch>
+        <Route exact path="/" render={() => "feed"} />
+        <Route exact path="/explore" render={() => "explore"} />
+    </Switch>
+);
+
+const PublicRoutes = props => (
+    <Switch>
+        <Route exact path="/" component={Auth} />
+        <Route exact path="/forgot" render={() => "password"} />
+    </Switch>
+);
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
+import store, { history } from "redux/configureStore";
+import I18n from "redux-i18n";
+import { translations } from "translations";
+import App from "components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+        <I18n translations={translations} initialLang="en" fallbackLang="en">
+            <App />
+        </I18n>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById("root")
+);
